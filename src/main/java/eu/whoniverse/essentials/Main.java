cdbcc89ebf37ee9dea40af410bbd1ae3497c57d2
xTheDoctah <alessio.caldarigi@gmail.com>
2018-08-21 21:47:18 +0200

    Finished Teleport

--- src/main/java/eu/whoniverse/essentials/Main.java
+++ src/main/java/eu/whoniverse/essentials/Main.java
@@ -1,12 +1,13 @@
 package eu.whoniverse.essentials;
 
+import eu.whoniverse.essentials.commands.TpCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getCommand("tp").setExecutor(new TpCommand());
     }
 
     @Override
---
+++ src/main/java/eu/whoniverse/essentials/commands/TpCommand.java
@@ -0,0 +1,43 @@
+package eu.whoniverse.essentials.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TpCommand implements CommandExecutor {
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            switch (strings.length) {
+                case 0:
+                    player.sendMessage("Args missing!");
+                    break;
+                case 1:
+                    Player player1 = Bukkit.getPlayer(strings[0]);
+                    if (player1 == null) {
+                        player.sendMessage("Player not exist or not online");
+                    } else {
+                        player.sendMessage("Teleporting...");
+                        player.teleport(player1);
+                    }
+                    break;
+                case 2:
+                    player1 = Bukkit.getPlayer(strings[0]);
+                    Player player2 = Bukkit.getPlayer(strings[1]);
+                    if (player1 == null || player2 == null) {
+                        player.sendMessage("One or more players do not exist.");
+                    } else {
+                        player.sendMessage(player.getName() + " is teleporting you to" + player2.getName());
+                        player1.teleport(player2);
+                    }
+                    break;
+                default:
+                    player.sendMessage("Sorry! Max 2 Args on this command.");
+                    break;
+            }
+        }
+        return true;
+    }
+}
