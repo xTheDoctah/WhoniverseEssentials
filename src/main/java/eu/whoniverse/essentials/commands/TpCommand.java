e660ea879f9166bee71e44027c2c39200d26018a
xTheDoctah <alessio.caldarigi@gmail.com>
2018-08-21 22:19:30 +0200

    Fixed Console performing TP

--- src/main/java/eu/whoniverse/essentials/commands/TpCommand.java
+++ src/main/java/eu/whoniverse/essentials/commands/TpCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class TpCommand implements CommandExecutor {
@@ -12,7 +13,7 @@ public class TpCommand implements CommandExecutor {
             Player player = (Player) commandSender;
             switch (strings.length) {
                 case 0:
-                    player.sendMessage("Args missing!");
+                    player.sendMessage("Args are missing!");
                     break;
                 case 1:
                     Player player1 = Bukkit.getPlayer(strings[0]);
@@ -36,6 +37,26 @@ public class TpCommand implements CommandExecutor {
                 default:
                     player.sendMessage("Sorry! Max 2 Args on this command.");
                     break;
+            }
+        } else if (commandSender instanceof ConsoleCommandSender) {
+            ConsoleCommandSender consoleCommandSender = (ConsoleCommandSender) commandSender;
+            switch (strings.length) {
+                case 0:
+                    consoleCommandSender.sendMessage("Args are missing!");
+                    break;
+                case 2:
+                    Player player1 = Bukkit.getPlayer(strings[0]);
+                    Player player2 = Bukkit.getPlayer(strings[1]);
+                    if (player1 == null || player2 == null) {
+                        commandSender.sendMessage("One or more players do not exist.");
+                    } else {
+                        commandSender.sendMessage("The server is teleporting you to" + player2.getName());
+                        player1.teleport(player2);
+                    }
+                    break;
+                default:
+                    consoleCommandSender.sendMessage("Sorry! U can teleport only one player to another!");
+
             }
         }
         return true;
