6e3b43f18382d366d0131b45df9209c6672150e8
xTheDoctah <alessio.caldarigi@gmail.com>
2018-08-21 21:15:29 +0200

    First commit

---
+++ src/main/java/eu/whoniverse/essentials/Main.java
@@ -0,0 +1,17 @@
+package eu.whoniverse.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
