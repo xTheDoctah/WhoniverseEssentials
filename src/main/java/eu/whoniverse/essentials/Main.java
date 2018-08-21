0a2faf8b0da9d6d66c347e4fbb7cac4d3adc143f
xTheDoctah <alessio.caldarigi@gmail.com>
2018-08-21 22:09:54 +0200

    Finished TeleportHere

--- src/main/java/eu/whoniverse/essentials/Main.java
+++ src/main/java/eu/whoniverse/essentials/Main.java
@@ -1,6 +1,7 @@
 package eu.whoniverse.essentials;
 
 import eu.whoniverse.essentials.commands.TpCommand;
+import eu.whoniverse.essentials.commands.TphereCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,6 +9,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("tp").setExecutor(new TpCommand());
+        getCommand("tphere").setExecutor(new TphereCommand());
     }
 
     @Override
---
+++ src/main/java/eu/whoniverse/essentials/commands/TphereCommand.java
@@ -0,0 +1,35 @@
+package eu.whoniverse.essentials.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+public class TphereCommand implements CommandExecutor {
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            switch (strings.length) {
+                case 0:
+                    player.sendMessage("Args missing!");
+                    break;
+                default:
+                    Arrays.asList(strings).forEach(item -> {
+                        if (Bukkit.getPlayer(item) == null) {
+                            player.sendMessage(item + " does not exists!");
+                        } else {
+                            Bukkit.getPlayer(item).sendMessage("U're gonna be teleported to " + player.getName());
+                            Bukkit.getPlayer(item).teleport(player);
+                        }
+                    });
+                    break;
+            }
+        }else{
+            commandSender.sendMessage("Only a player can perform this command!");
+        }
+        return true;
+    }
+}
